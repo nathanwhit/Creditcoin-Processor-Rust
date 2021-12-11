fd1eb8013c69671f85c7bff9617b15405a93cba1
Nathan Whitaker <nathan.whitaker01@gmail.com>
2021-12-11 16:07:18 -0500

    Don't error on overflow in housekeeping (backwards compat)

--- src/handler.rs
+++ src/handler.rs
@@ -2145,6 +2145,18 @@ fn verify_gateway_signer(my_sighash: &str, ctx: &mut HandlerContext) -> TxnResul
     );
 }
 
+macro_rules! subtract_or_return {
+    ($e: expr) => {
+        match $e {
+            Ok(d) => d,
+            Err(e) => {
+                log::warn!("{:?}", e);
+                return Ok(());
+            }
+        }
+    };
+}
+
 impl CCTransaction for Housekeeping {
     fn execute(
         self,
@@ -2207,7 +2219,7 @@ impl CCTransaction for Housekeeping {
         filter(tip, tx_ctx, &ask, |addr, proto| {
             let ask_order = protos::AskOrder::try_parse(proto)?;
             let start = BlockNum::try_from(&ask_order.block)?;
-            let elapsed = (block_idx - start)?;
+            let elapsed = subtract_or_return!(block_idx - start);
             if ask_order.expiration < elapsed {
                 tx_ctx.delete_state_entry(addr)?;
             }
@@ -2218,7 +2230,7 @@ impl CCTransaction for Housekeeping {
         filter(tip, tx_ctx, &bid, |addr, proto| {
             let bid_order = protos::BidOrder::try_parse(proto)?;
             let start = BlockNum::try_from(&bid_order.block)?;
-            let elapsed = (block_idx - start)?;
+            let elapsed = subtract_or_return!(block_idx - start);
             if bid_order.expiration < elapsed {
                 tx_ctx.delete_state_entry(addr)?;
             }
@@ -2229,7 +2241,7 @@ impl CCTransaction for Housekeeping {
         filter(tip, tx_ctx, &offer, |addr, proto| {
             let offer = protos::Offer::try_parse(proto)?;
             let start = BlockNum::try_from(&offer.block)?;
-            let elapsed = (block_idx - start)?;
+            let elapsed = subtract_or_return!(block_idx - start);
             if offer.expiration < elapsed {
                 tx_ctx.delete_state_entry(addr)?;
             }
@@ -2240,7 +2252,7 @@ impl CCTransaction for Housekeeping {
         filter(tip, tx_ctx, &deal, |addr, proto| {
             let deal_order = protos::DealOrder::try_parse(proto)?;
             let start = BlockNum::try_from(&deal_order.block)?;
-            let elapsed = (block_idx - start)?;
+            let elapsed = subtract_or_return!(block_idx - start);
             if deal_order.expiration < elapsed && deal_order.loan_transfer.is_empty() {
                 if tip > DEAL_EXP_FIX_BLOCK {
                     let wallet_id = string!(NAMESPACE_PREFIX, WALLET, &deal_order.sighash);
@@ -2263,7 +2275,7 @@ impl CCTransaction for Housekeeping {
         filter(tip, tx_ctx, &repay, |addr, proto| {
             let repayment_order = protos::RepaymentOrder::try_parse(proto)?;
             let start = BlockNum::try_from(&repayment_order.block)?;
-            let elapsed = (block_idx - start)?;
+            let elapsed = subtract_or_return!(block_idx - start);
             if repayment_order.expiration < elapsed && repayment_order.previous_owner.is_empty() {
                 tx_ctx.delete_state_entry(addr)?;
             }
@@ -2274,7 +2286,7 @@ impl CCTransaction for Housekeeping {
         filter(tip, tx_ctx, &fee, |addr, proto| {
             let fee = protos::Fee::try_parse(proto)?;
             let start = BlockNum::try_from(&fee.block)?;
-            let elapsed = (block_idx - start)?;
+            let elapsed = subtract_or_return!(block_idx - start);
 
             if elapsed > YEAR_OF_BLOCKS {
                 let wallet_id = string!(NAMESPACE_PREFIX, WALLET, &fee.sighash);
