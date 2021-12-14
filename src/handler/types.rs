d383d5449c4e57d60762b43f8d8cdb23ce09bb23
Nathan Whitaker <nathan.whitaker01@gmail.com>
2021-12-14 11:48:46 -0500

    BlockNum subtraction produces BlockInterval

--- src/handler/types.rs
+++ src/handler/types.rs
@@ -552,26 +552,21 @@ impl Add<u64> for BlockNum {
     }
 }
 impl Sub<u64> for BlockNum {
-    type Output = TxnResult<BlockNum>;
+    type Output = BlockInterval;
 
     fn sub(self, rhs: u64) -> Self::Output {
-        Ok(Self(self.0.checked_sub(rhs).ok_or_else(|| {
-            CCApplyError::InvalidTransaction(
-                "The subtraction would have resulted in overflow".into(),
-            )
-        })?))
+        let rhs = BlockNum(rhs);
+        self.sub(rhs)
     }
 }
 
 impl Sub<BlockNum> for BlockNum {
-    type Output = TxnResult<BlockNum>;
+    type Output = BlockInterval;
 
     fn sub(self, rhs: BlockNum) -> Self::Output {
-        Ok(Self(self.0.checked_sub(rhs.0).ok_or_else(|| {
-            CCApplyError::InvalidTransaction(
-                "The subtraction would have resulted in overflow".into(),
-            )
-        })?))
+        let diff = self.0 - rhs.0;
+        let interval = i128::from(diff);
+        BlockInterval(interval)
     }
 }
 
